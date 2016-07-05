7a356e56e7ce7e44a070f59514ad249ccfc4277e
at-cuclt <cuc12cntt@gmail.com>
2016-07-05 08:42:41 +0700

    Edit Book_item model

--- app/Book_item.php
+++ app/Book_item.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Book_item extends Model
 {
 	protected $table = 'book_items';
-	protected $fillable = ['book_id', 'admin_user_id', 'created_at', 'updated_at'];
+	protected $fillable = ['book_id', 'created_at', 'updated_at'];
 }
