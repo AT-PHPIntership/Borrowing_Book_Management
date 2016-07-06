0ddc8230093ec46870b3d0aa2f5b18c0d7f51380
at-cuclt <cuc12cntt@gmail.com>
2016-07-06 15:42:24 +0700

    Create seeder BorrowDetails

--- database/seeds/BorrowDetailsTableSeeder.php
+++ database/seeds/BorrowDetailsTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class BookDetailsTableSeeder extends Seeder
+class BorrowDetailsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,16 @@ class BookDetailsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        for($i = 0; $i < 50; $i++){
+            DB::table('borrow_details')->insert([
+                'borrow_id'       => rand(1,10),
+                'book_item_id'    => rand(1,100),
+                'status'          => rand(0,1),
+                'expiretime'      => $faker->date,
+                'created_at'      => Carbon\Carbon::now(),              
+            ]);
+        }
     }
 }
