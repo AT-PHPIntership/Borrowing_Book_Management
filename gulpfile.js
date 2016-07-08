8c3d38c246a9fab2ef269e7b25057389060e378d
at-dongvd <vddongbk@gmail.com>
2016-07-08 11:51:42 +0700

    setup bower

--- gulpfile.js
+++ gulpfile.js
@@ -14,20 +14,20 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     // Combine scripts
-  mix.scripts([
-      'jquery/dist/jquery.js',
-      'bootstrap/dist/js/bootstrap.min.js'
+  mix.scripts([ 
+      'vendors/jquery/dist/jquery.min.js',
+      'vendors/bootstrap/dist/js/bootstrap.min.js'
     ],
     'public/backend/js/vendor.js',
-    'vendor/bower_dl'
+    'vendor/bower_dl/gentelella'
   );
 
   // Compile css
   mix.styles([
-  	  'bootstrap/dist/css/bootstrap.min.css',
-  	  'font-awesome/css/font-awesome.min.css'
+  	  'vendors/bootstrap/dist/css/bootstrap.min.css',
+  	  'vendors/font-awesome/css/font-awesome.min.css'  
   ], 
     'public/backend/css/vendor.css',
-    'vendor/bower_dl'
-);
+    'vendor/bower_dl/gentelella'
+  );
 });
