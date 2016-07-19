b3ea6be306c077fbf7d573d4278091740647d626
at-dongvd <vddongbk@gmail.com>
2016-07-19 10:26:09 +0700

    fix CI

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -74,7 +74,8 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id id
+     *
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
