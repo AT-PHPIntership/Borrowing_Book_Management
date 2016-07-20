5e311fe5bc6993e1e570750f2dc884a2a7ef29df
at-dongvd <vddongbk@gmail.com>
2016-07-20 11:06:22 +0700

    edit js for model confirm

--- public/backend/js/admins.js
+++ public/backend/js/admins.js
@@ -8,19 +8,19 @@ $(document).ready(function(){
   $('#confirmDelete').on('show.bs.modal', function (e) {
   	  // set message
       $message = $(e.relatedTarget).attr('data-message');
-      $(this).find('.modal-body p').text($message);
+      $('.modal-body p').text($message);
       // set title for model
       $title = $(e.relatedTarget).attr('data-title');
-      $(this).find('.modal-title').text($title);
+      $('.modal-title').text($title);
 
       // Pass form reference to modal for submission on yes/ok
       var form = $(e.relatedTarget).closest('form');
-      $(this).find('.modal-footer #confirm').data('form', form);
+      $('.modal-footer #confirm').data('form', form);
   });
  
       //Form confirm (yes/ok) handler, submits form
-  $('#confirmDelete').find('.modal-footer #confirm').on('click', function(){
-      $(this).data('form').submit();
+  $('#confirmDelete .modal-footer #confirm').on('click', function(){
+      $('form').submit();
   });
 
   //countdown shutdown alert
