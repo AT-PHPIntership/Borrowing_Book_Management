9b10876e99ad8faeebf8c4e0b81f19f9d9f444de
at-dongvd <vddongbk@gmail.com>
2016-07-28 16:34:58 +0700

    fix hardcode

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -20,9 +20,9 @@ class IndexController extends Controller
     public function index()
     {
         $categories = Book::select('category_id')->distinct()->get();
-        $books = Book::paginate(9);
-        $images = Book::select('image')->take(3)->get();
-        $imagedefault = Book::select('image')->take(1)->get();
+        $books = Book::paginate(config('define.number_pages'));
+        $images = Book::select('image')->take(config('define.number_images_slide'))->get();
+        $imagedefault = Book::select('image')->take(config('define.number_image_default'))->get();
 
         return view('frontend.index', compact('categories', 'books', 'images', 'imagedefault'));
     }
@@ -53,10 +53,10 @@ class IndexController extends Controller
      */
     public function filter($id)
     {
-        $books = Book::select('id', 'name', 'category_id', 'author', 'publish_year', 'number_of_page', 'quantity', 'image')->where('category_id', $id)->paginate(9);
+        $books = Book::select('id', 'name', 'category_id', 'author', 'publish_year', 'number_of_page', 'quantity', 'image')->where('category_id', $id)->paginate(config('define.number_pages'));
         if (!empty($books)) {
-            $imagedefault = Book::select('image')->take(1)->get();
-            $images = Book::select('image')->take(3)->get();
+            $imagedefault = Book::select('image')->take(config('define.number_image_default'))->get();
+            $images = Book::select('image')->take(config('define.number_images_slide'))->get();
             $categories = Book::select('category_id')->distinct()->get();
             
             return view('frontend.index', compact('books', 'categories', 'images', 'imagedefault'));
