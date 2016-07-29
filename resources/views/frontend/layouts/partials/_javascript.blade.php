2032284db96223a235f2dd4412caf31078a065fb
at-dongvd <vddongbk@gmail.com>
2016-07-29 11:24:57 +0700

    setup bower v2

--- resources/views/frontend/layouts/partials/_javascript.blade.php
+++ resources/views/frontend/layouts/partials/_javascript.blade.php
@@ -1,7 +1,6 @@
 <!-- COMBINE BOOTSTRAP AND JQUERY JS -->
     <script src="{{ url('backend/js/vendor.js') }}"></script>  
     <script src="{{ url('frontend/js/upload.js') }}"></script>
-    <script type="text/javascript" src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
     <script type="text/javascript">
     	var pathjsonsearch = {!! json_encode(config('path.pathjsonsearch')) !!};
     	var timeout = {!! json_encode(config('define.timeout')) !!};
